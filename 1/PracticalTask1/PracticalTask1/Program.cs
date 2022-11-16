c209a71d16b157280a4ad49eb508b42aa0afac13
Filipp <ardos127@gmail.com>
2022-11-16 20:39:23 +0300

    Start here task

---
+++ 1/PracticalTask1/PracticalTask1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+class MaxNum
+{
+    static void Main()
+    {
+        string userInput; int amount, maxAmount;
+        Console.WriteLine("Type \"break\" to exit.");
+        do
+        {
+            amount = 0; maxAmount = 0;
+            userInput = Console.ReadLine();
+            if (userInput.Length > 0)
+            {
+                amount = 1;
+                for (int i = 0; i < userInput.Length - 1; i++)
+                {
+                    if (userInput[i] != userInput[i + 1])
+                    {
+                        amount++;
+                    }
+                    else
+                    {
+                        if (amount > maxAmount)
+                        {
+                            maxAmount = amount;
+                        }
+                        amount = 1;
+                    }
+                }
+                if (amount > maxAmount)
+                {
+                    maxAmount = amount;
+                }
+                if (maxAmount == 1)
+                {
+                    maxAmount = 0;
+                }
+            }
+            Console.WriteLine("Maximum number of unequal consecutive characters is: " +maxAmount);
+        }
+        while(userInput != "break" && userInput != null);
+    }
+}
